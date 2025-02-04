7d29ae202e08668e5e6216b388688e900b14e6b4
B0811 <typhloziken@gmail.com>
2025-02-04 13:57:17 -0500

    make enemy passive movement + start being hit

--- Scripts/enemyboot.gd
+++ Scripts/enemyboot.gd
@@ -6,13 +6,24 @@ var inrange = false
 
 @export var target_to_chase: CharacterBody2D
 
+var timer = 0
+
+var health = 5
+
+var rng = RandomNumberGenerator.new()
+
+
 const SPEED = 200.0
 
+func _ready():
+	rng.randomize()
+	
 func _physics_process(delta):
 	if inrange == true:
 		nav_agent_.target_position = target_to_chase.global_position
 		velocity = global_position.direction_to(nav_agent_.get_next_path_position()) * SPEED
-		
+	
+	
 	
 	move_and_slide()
 
@@ -26,3 +37,32 @@ func _on_area_2d_area_entered(area):
 	if area.is_in_group("players") :
 		inrange = true
 		print("Player has been found")
+		
+
+
+
+
+func _on_area_2d_area_exited(area):
+	if area.is_in_group("players"):
+		velocity = Vector2.ZERO
+		inrange = false
+		print("player has been lost")
+
+
+func _on_timer_timeout():
+	if inrange == false and timer == 0:
+			var random_number = randi()% 4
+			match random_number:
+				0: position.x += 10
+				1: position.x -= 10
+				2: position.y += 10
+				3: position.y -= 10
+
+
+
+
+
+func _on_hitboxarea_area_entered(area):
+	if area.is_in_group("projectile"):
+		health -= 1
+		print("Hit!")
