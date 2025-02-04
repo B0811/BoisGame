7e42a070752e296b0907199c437d379c3cd46b13
B0811 <typhloziken@gmail.com>
2025-02-04 12:48:58 -0500

    make chase logic work properly when in range

--- Scripts/enemyboot.gd
+++ Scripts/enemyboot.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var inrange = false
 
 @onready var nav_agent_ = $NavigationAgent2D
 
@@ -8,7 +9,20 @@ extends CharacterBody2D
 const SPEED = 200.0
 
 func _physics_process(delta):
-	nav_agent_.target_position = target_to_chase.global_position
-	velocity = global_position.direction_to(nav_agent_.get_next_path_position()) * SPEED
+	if inrange == true:
+		nav_agent_.target_position = target_to_chase.global_position
+		velocity = global_position.direction_to(nav_agent_.get_next_path_position()) * SPEED
+		
 	
 	move_and_slide()
+
+
+
+
+
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("players") :
+		inrange = true
+		print("Player has been found")
