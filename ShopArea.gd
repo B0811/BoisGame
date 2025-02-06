6fb66366812850771be1fd8b186b1e9d6346c667
MrJegson <jackhubbard49@gmail.com>
2025-02-05 19:54:12 -0500

    Revert "add early stage of upgrade shop"

--- ShopArea.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-@onready var control_2 = $"../Control2"
-
-
-
-func _on_area_entered(area):
-	if area.is_in_group("players"):
-		control_2.visible = true
--- hideoutenter.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-
-
-func _on_area_entered(area):
-	if area.is_in_group("players"):
-		print("Entering Hideout...")
-		get_tree().change_scene_to_file("res://hideoutinside.tscn")
